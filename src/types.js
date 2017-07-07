268bd3446ddc64482181d4780b9fc42fcd18ba73
Jim Jeffers <jim@sumocreations.com>
2017-07-07 15:39:09 -0700

    Removed unused payload API symbol and Headers type.
    Reducing the responsibility of this middleware to simply checking and obtaining a valid token.

--- src/types.js
+++ src/types.js
@@ -1,9 +1,6 @@
 // @flow
 
-export type Headers = { [string]: string }
-
 export type Config = {
-  apiPayloadSymbol: Symbol,
   currentAccessToken: (state: ?any) => string,
   handleAuthenticationError: (error: any, store: ?any) => void,
   currentRefreshToken: ?(state: ?any) => string,

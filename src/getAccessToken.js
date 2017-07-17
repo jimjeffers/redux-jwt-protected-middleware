51a43b415d0b1d1c63af4577feea94db23503f1c
Jim Jeffers <jim@sumocreations.com>
2017-07-17 10:51:19 -0700

    Token refresh utilizes generator based async polling to prevent race conditions.

--- src/getAccessToken.js
+++ src/getAccessToken.js
@@ -1,6 +1,85 @@
 //@flow
 import { validateToken, isDefined, isBlank } from "./helpers"
-import type { Config } from "./types"
+import type { Config, FetchArguments } from "./types"
+
+/**
+ * The crux of the middleware. This generator runs in an infinite loop
+ * and obtains our middleware configuration and store via the params passed
+ * to the next() iterator function. It will update it's internal state and
+ * yeild a blank ("") string if it is currently fetching a new token.
+ */
+function* fetchToken(): Generator<string, void, ?FetchArguments> {
+  var isFetching: boolean = false
+  var token: string = ""
+  while (true) {
+    const args = yield ""
+    if (args) {
+      const { config, store } = args
+      const {
+        currentAccessToken,
+        handleRefreshAccessToken,
+        currentRefreshToken
+      } = config
+      token = currentAccessToken(store)
+      if (isBlank(token) || !validateToken(token)) {
+        if (handleRefreshAccessToken && currentRefreshToken) {
+          isFetching = true
+          try {
+            const refreshToken = currentRefreshToken(store)
+            handleRefreshAccessToken(refreshToken, store)
+            token = currentAccessToken(store)
+          } catch (error) {
+            config.handleAuthenticationError(error, store)
+          }
+          isFetching = false
+        } else {
+          config.handleAuthenticationError(
+            new Error("Access token is no longer valid"),
+            store
+          )
+        }
+      }
+      yield isFetching ? "" : token
+    }
+  }
+}
+
+/**
+ * A local instance of our generator function which we'll poll for
+ * an available token.
+ */
+const gen = fetchToken()
+gen.next() // Ensure the generator is now yielding.
+
+/**
+ * Recursively polls for a token using recursion and async function
+ * calls.
+ * @param {FetchArguments} args The optional redux store and middleware configuration.
+ * @returns {string} A token value once a value has been obtained.
+ */
+function getToken(args: FetchArguments): Promise<?string> {
+  return new Promise((resolve, reject) => {
+    const token = gen.next(args).value
+    if (token !== "") {
+      resolve(token)
+    } else {
+      setTimeout(() => resolve(gen.next(args).value), 100)
+    }
+  })
+}
+
+/**
+ * Recursively polls for a token using recursion and async function
+ * calls.
+ * @param {FetchArguments} args The optional redux store and middleware configuration.
+ * @returns {string} A token value once a value has been obtained.
+ */
+const checkForToken = async function(
+  args: FetchArguments
+): string | Promise<?string> {
+  const token = await getToken(args)
+  return !isBlank(token) ? token : checkForToken(args)
+}
 
 /**
  * A helper method that creates a promise which performs a flow similar
@@ -11,25 +90,7 @@ import type { Config } from "./types"
  * @return {Function}     An async function that returns an accessible access token.
  */
 const getAccessToken = (config: Config) => async (store: any) => {
-  const { currentAccessToken, handleRefreshAccessToken, currentRefreshToken } = config
-  const token: string = currentAccessToken(store)
-  if (isBlank(token) || !validateToken(token)) {
-    if (handleRefreshAccessToken && currentRefreshToken) {
-      try {
-        await handleRefreshAccessToken(currentRefreshToken(store), store)
-        return currentAccessToken(store)
-      } catch(error) {
-        config.handleAuthenticationError(error, store)
-      }
-    } else {
-      config.handleAuthenticationError(
-        new Error("Access token is no longer valid"),
-        store
-      )
-    }
-  } else {
-    return token
-  }
+  return await checkForToken({ config, store })
 }
 
 export default getAccessToken
